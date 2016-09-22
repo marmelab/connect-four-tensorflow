2ced8a87db1fca6b4cdd64147b6200f1978a7a46
Florian FERBACH <florian@marmelab.com>
2016-09-22 14:03:23 +0200

    Add tests on is_aligned

---
+++ ai/test_reward.py
@@ -0,0 +1,84 @@
+import unittest
+import tensorflow as tf
+
+from reward import *
+from bitmasks import *
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_is_aligned_detects_2nd_row(self):
+        with tf.Session() as session:
+            session.run(tf.initialize_all_variables())
+
+            game_board = [
+                [0, 0, 0, 0],
+                [1, 1, 1, 1],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+            ]
+
+            self.assertEqual(
+                session.run(is_aligned(), feed_dict={
+                    board: game_board,
+                    mask: bitmasks[3]
+                }),
+                True
+            )
+
+    def test_is_aligned_detects_2nd_column(self):
+        with tf.Session() as session:
+            session.run(tf.initialize_all_variables())
+
+            game_board = [
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+            ]
+
+            self.assertEqual(
+                session.run(is_aligned(), feed_dict={
+                    board: game_board,
+                    mask: bitmasks[7]
+                }),
+                True
+            )
+
+    def test_is_aligned_detects_1st_diagonal(self):
+        with tf.Session() as session:
+            session.run(tf.initialize_all_variables())
+
+            game_board = [
+                [1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1],
+            ]
+
+            self.assertEqual(
+                session.run(is_aligned(), feed_dict={
+                    board: game_board,
+                    mask: bitmasks[0]
+                }),
+                True
+            )
+
+    def test_is_aligned_do_not_detect_false_positive(self):
+        with tf.Session() as session:
+            session.run(tf.initialize_all_variables())
+
+            game_board = [
+                [1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1],
+            ]
+
+            self.assertEqual(
+                session.run(is_aligned(), feed_dict={
+                    board: game_board,
+                    mask: bitmasks[1]
+                }),
+                False
+            )
