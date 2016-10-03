dcc380c29bc9b868ef8fd6284fe4782be1319b2d
Florian FERBACH <florian@marmelab.com>
2016-10-03 09:00:36 +0200

    Change from manual to automatic back propagation

--- ai/neural_network/neural_network.py
+++ ai/neural_network/neural_network.py
@@ -1,53 +1,127 @@
 import tensorflow as tf
 
-from bitmasks import bitmasks
+from copy import deepcopy
 
-masks = tf.constant(bitmasks, 'bool', [10, 4, 4])
-ai_player = tf.constant(1, 'float')
+board_width = 4
+board_height = 4
+
+# Parameters
+learning_rate = 0.1
+
+# Network Parameters
+n_hidden_1 = 10
+n_hidden_2 = 10
+n_input = board_width * board_height
+n_classes = board_width
+
+# tf Graph input
+board = tf.placeholder("float", [4, 4])
+x = tf.reshape(board, [1, n_input])
+rating = tf.placeholder("float", [4])
+y = tf.reshape(rating, [1, n_classes])
 
 
-def play(inputs, output):
-    # get column from board
-    column_index = tf.to_int32(output)
-    column = tf.slice(tf.reshape(inputs, [4, 4]), [column_index[0], 0], [1, 4])
-    # check if empty
-    boolean_column = tf.equal(column, 0)
-    # get lower empty row
-    empty_rows = tf.where(boolean_column)
-    lower_row = tf.argmax(empty_rows, 1)
-    # update board
-    row_index = tf.to_int32(lower_row[0])
-    return tf.scatter_update(inputs, column_index[0] * 4 + row_index, ai_player)
+def multilayer_network(x, weights, biases):
+    # Hidden layer with RELU activation
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(layer_1)
+    # Hidden layer with RELU activation
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
+    # Output layer with linear activation
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    return out_layer
+
+# Store layers weight & bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+}
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+# Construct model
+predict = multilayer_network(x, weights, biases)
+
+# Backward propagation
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(predict, y))
+
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+saver = tf.train.Saver()
 
 
-def get_board_reward(board):
-    board = tf.reshape(board, [4, 4])
+class NeuralNetwork:
 
-    # board = tf.placeholder('float', [4, 4])
-    reward_positive = lambda: tf.constant(1, 'float')
-    reward_negative = lambda: tf.constant(-1, 'float')
-    reward_none = lambda: tf.constant(0, 'float')
+    def __init__(self, player):
+        self.session = tf.Session()
+        self.session.run(tf.initialize_all_variables())
+        self.player = player
+        self.saved_actions = []
+        try:
+            saver.restore(
+                self.session, 'ai/neural_network/saved_graphs/connect-four')
+            print("Restored graph file")
+        except ValueError:
+            print("Cannot restore graph file")
+        self.number_of_games = 0
 
-    # put board and mask in 1d
-    flattened_masks = tf.reshape(masks, [10 * 4 * 4])
-    flattened_boards = tf.reshape(tf.tile(board, [10, 1]), [10 * 4 * 4])
+    def __enter__(self):
+        return self
 
-    # use a boolean mask to extract a particular chunk
-    extract_chunks = tf.boolean_mask(flattened_boards, flattened_masks)
+    def next_move(self, game):
+        predictions = self.session.run([predict], feed_dict={
+            board: game.board,
+        })
+        legal_moves = game.get_legal_moves()
 
-    # go back to 2d
-    reconstruct_chunks = tf.reshape(extract_chunks, [10, 4])
+        score = None
+        column = -1
+        for index, prediction in enumerate(predictions[0][0]):
+            if prediction > score and legal_moves[index]:
+                score = prediction
+                column = index
 
-    # which of the chunks have 4 consecutive 1s ?
-    consecutive_chunks = tf.reduce_sum(reconstruct_chunks, 1)
+        self.saved_actions.append({
+            'board': deepcopy(game.board),
+            'column': column,
+        })
 
-    # game is lost if any of the chunks sum is -4 (-1 + -1 + -1 + -1)
-    opponent_has_consecutive_chunks = tf.equal(consecutive_chunks, -4)
-    is_game_lost = tf.reduce_any(opponent_has_consecutive_chunks)
+        return column
 
-    # game is lost if any of the chunks sum is 4 (1 + 1 + 1 + 1)
-    player_has_consecutive_chunks = tf.equal(consecutive_chunks, 4)
-    is_game_won = tf.reduce_any(player_has_consecutive_chunks)
+    def turn_feedback(self, player, column):
+        pass
 
-    return tf.case(
-        {is_game_won: reward_positive, is_game_lost: reward_negative}, default=reward_none, exclusive=True)
+    def game_feedback(self, game, status, winner):
+        self.number_of_games += 1
+
+        if winner != 0:
+            if winner == self.player:
+                rating = 1
+
+                for action in self.saved_actions:
+                    self.back_propagation(
+                        action['board'], action['column'], 1)
+
+        self.saved_actions = []
+
+        if self.number_of_games % 1000 == 0:
+            saver.save(self.session,
+                       'ai/neural_network/saved_graphs/connect-four')
+            print("Saving session graph")
+
+    def back_propagation(self, game_board, column, score):
+        output_data = [0., 0., 0., 0.]
+        output_data[column] = score
+
+        _, c = self.session.run([optimizer, cost], feed_dict={
+            board: game_board,
+            rating: output_data
+        })
+
+    def __exit__(self, type, value, traceback):
+        self.session.close()
